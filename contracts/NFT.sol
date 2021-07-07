cbcf66dcb1f3d9a9b79c455779e25a04c311b0fe
SamWarden <SamWardenSad@gmail.com>
2021-07-07 23:47:16 +0300

    Fix NFT

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,27 +1,24 @@
-//Contract based on https://docs.openzeppelin.com/contracts/3.x/erc721
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
-​
+pragma solidity ^0.6.6;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-​
+
 contract NFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-​
-    constructor(string name, string symbol) public ERC721(name, symbol) {}
-​
+
+    constructor(string memory name, string memory symbol) public ERC721(name, symbol) {}
+
     function mint(string memory tokenURI)
         public
-        returns (uint256)
+        returns (uint256 newItemId)
     {
         _tokenIds.increment();
-​
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
+
+        newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
-​
-        return newItemId;
     }
 }
